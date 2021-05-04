0b73f16423e126b7b39d98c1f83b53c4e8dcd08e
ilhantekir <info@ilhantekir.com>
2021-05-04 15:41:44 +0300

    iframe test

---
+++ pages/iframe.js
@@ -0,0 +1,9 @@
+export default function iframe() {
+  return (
+    <iframe
+      src="https://erka.mi4biz.com/Customer/AnonymousIssueEntry.aspx?BusinessChannelID=TURwZ3BML29BOVljczhKN3ovYmdQUT090&amp;q=eHFPUWJBODNzTWhwY09VVnpOZmR0QjJINkVza2F6NmxsYmx6em03UGxNTnlYTVU5ejJia1pZRjZaT050YWFlM0ZGNFh2bWhlRmZqVGlHOXl5ZVRIY1E9PQ2"
+      height="600px"
+      width="100%"
+    ></iframe>
+  );
+}
